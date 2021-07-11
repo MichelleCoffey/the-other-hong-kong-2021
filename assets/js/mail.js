ff58b34da9354d75b5d90460244ebe56c4e547f4
MichelleCoffey <michellembcoffey@icloud.com>
2021-07-11 12:48:37 +0000

    added js for email.js.

---
+++ assets/js/mail.js
@@ -0,0 +1,25 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_2m0k8f5';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+function clearForm() {
+        document.getElementById('for_name').value = '';
+        document.getElementById('email_address').value = '';
+    }
