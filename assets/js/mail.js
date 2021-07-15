f75413c0f096e9be36aee178964b96c393481e73
MichelleCoffey <michellembcoffey@icloud.com>
2021-07-15 01:29:07 +0000

    removed images and fixed navbarresponsive issues.

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     document.getElementById('form').addEventListener('submit', function(event) {
         event.preventDefault();
-        document.getElementById("form").reset();
+        
 
     emailjs.sendForm('service_951emqu', 'template_upi7stc', this)
        .then(function() {
